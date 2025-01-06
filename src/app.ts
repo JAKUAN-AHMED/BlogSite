81ee7d047179f74c4e6aab12365f005beda905fd
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2025-01-06 09:51:50 +0600

    project set up successfully done

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import router from './routes/main';
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cookieParser());
+
+//test route
+const test = async (req: Request, res: Response) => {
+  const sayHi = 'Hi~! Jakuan';
+  res.send(sayHi);
+};
+
+app.get('/', test);
+
+//application routes
+app.use('/api/v1', router);
+
+//gloabal err handler
+// app.use(globalErrorHandler);
+
+//Not Found Route
+// app.use(NotFound);
+
+export default app;
