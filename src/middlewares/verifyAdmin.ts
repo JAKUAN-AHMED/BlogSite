809a6704a6a06d275f790e93c9dbd8128be2fc96
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2025-01-26 16:41:17 +0600

    updated authentication system

--- src/middlewares/verifyAdmin.ts
+++ src/middlewares/verifyAdmin.ts
@@ -1,7 +1,6 @@
 import { NextFunction ,Request,Response} from "express";
 
 export const verifyAdmin=async(req:Request,res:Response,next:NextFunction)=>{
-    console.log('user role',req.user);
     if(req.user?.role!=="admin"){   
             res.status(403).json({
             success:false,
